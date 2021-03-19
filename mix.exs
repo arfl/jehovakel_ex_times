28da68343d2a900987877ff09f1f28df9df31c3c
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2021-03-19 11:30:01 +0100

    Bump Version to 1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JehovakelExTimes.MixProject do
   def project do
     [
       app: :jehovakel_ex_times,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
