56d56e04a8ed564cb153937d1bfa4f5df388c17a
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2021-06-18 12:41:38 +0200

    Fix Tix Setup.

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule JehovakelExTimes.MixProject do
       {:timex, "~> 3.7"},
       {:jason, "~> 1.0", optional: true},
       {:excoveralls, ">= 0.10.5", only: :test},
-      {:tix, ">= 0.0.0", only: :test},
+      {:tix, ">= 0.0.0", only: :test, runtime: false},
       # Property based Testing for Elixir (based upon PropEr)
       {:propcheck, "~> 1.2", only: [:test]}
     ]
